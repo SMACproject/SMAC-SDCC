c28fe0ab36930c2dd08c50df2b1e06fa7995e4e4
Ekawahyu Susilo <ekawahyu@aisenke.com>
2014-06-26 13:58:38 -0500

    added blink example

---
+++ examples/README.md
@@ -0,0 +1,6 @@
+Examples
+========
+
+This directory contains generic and platform specific examples. A generic
+example can be built upon multiple platforms, while plaform specific
+application can only be built against one particular platform.
--- examples/blink/blink.c
+++ examples/blink/blink.c
@@ -1,21 +1,21 @@
 /*
- * main.c
+ * blink.c
  *
  * Created on: Jun 26, 2014
  *     Author: Ekawahyu Susilo
  *
  * Copyright (c) 2014, Chongqing Aisenke Electronic Technology Co., Ltd.
  * All rights reserved.
- *
+ * 
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
+ * modification, are permitted provided that the following conditions are met: 
+ * 
  * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
+ *    this list of conditions and the following disclaimer. 
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
+ *    and/or other materials provided with the distribution. 
+ * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -27,23 +27,10 @@
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- *
+ * 
  * The views and conclusions contained in the software and documentation are
  * those of the authors and should not be interpreted as representing official
  * policies, either expressed or implied, of the copyright holder.
- *
+ * 
  */
 
-#include "smac.h"
-#include "sys/clock.h"
-#include "dev/leds.h"
-
-int main (void)
-{
-  clock_init();
-  leds_init();
-  leds_off(LEDS_ALL);
-  io_arch_init();
-
-  user_main();
-}
